30259c112b49967816ab5c59eef4765c1cc2459c
Adam Groszer <agroszer@mail.com>
2010-02-08 15:17:17 +0000

    vb

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*rnames):
 
 setup(
     name = 'z3c.recipe.paster',
-    version = '0.5.3',
+    version = '0.5.4dev',
     author = 'Roger Ineichen and the Zope Community',
     author_email = 'zope-dev@zope.org',
     description = 'Zope3 paste deploy setup recipe',
